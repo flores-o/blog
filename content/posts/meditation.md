e3768f2893a8cac6ea4045c687f3a055ee774af8
Oana Florescu <oanaflores@fb.com>
2022-08-28 05:15:07 -0700

    update bio

---
+++ content/posts/meditation.md
@@ -0,0 +1,49 @@
+---
+title: "[Draft] Meditation"
+date: 2022-04-06
+draft: true
+---
+
+Meditation
+
+tldr best thing
+
+This is connected with what I think is the meaning of life. I think there are 2 types of purpose in life.
+
+My opinion. There is no ultimate purpose. To do good, go to haven or salvage the species. Why?
+
+Let's assume there is a predefined purpose in life, X. Then your life revolves around working for X, achieving X. Everything else loses meaning. If something does not contribute towards achieving X, then what is the point of it? Pfiu, such a tiring existance.
+
+Let's assume there is no ultimate purpose. Life has no predefined purpose. There is no point. I find this liberating. There is no point anyway, so you might enjoy the ride. Every moment it's meaningful for its own sake.
+
+The inner purpose (Also the primary purpose). This is to be present. To enjoy the moment as it is, be aware. [1*]
+The outer purpose (Or the secondary purpose). This is chosen by each one of us. Something to build or achieve in the outer world. It needs to be
+
+
+
+What are the benefits of meditation
+
+
+It makes me feel good and gives me choice.
+
+
+
+Helps me being present and aware. See primary purpose above.
+
+It gives me space and choice. Normally something is happening and there is an automatic reaction. When looking back, maybe I'd choose differently, but in the moment I just reacted.
+
+With meditation and space, I can see what is happening, take a breath and choose if I react and how.
+
+
+Focus
+
+
+I feel someone else has lived my life on auto pilot.
+
+
+
+
+
+---
+
+The importance of now. Now is the most important thing and the only thing there is. Everything that we experience is in the now.  Why
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 /**
  * Sets up Justified Gallery.
  */
+
 if (!!$.prototype.justifiedGallery) {
   var options = {
     rowHeight: 140,
@@ -10,18 +11,21 @@ if (!!$.prototype.justifiedGallery) {
   $(".article-gallery").justifiedGallery(options);
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
+
+
+  if (true) { console.log("trying to log smth here") }
 
   /**
    * Shows the responsive navigation menu on mobile.
    */
-  $("#header > #nav > ul > .icon").click(function() {
+  $("#header > #nav > ul > .icon").click(function () {
     $("#header > #nav > ul").toggleClass("responsive");
   });
 
 
   /**
-   * Controls the different versions of  the menu in blog post articles 
+   * Controls the different versions of  the menu in blog post articles
    * for Desktop, tablet and mobile.
    */
   if ($(".post").length) {
@@ -40,7 +44,7 @@ $(document).ready(function() {
     /**
      * Display the menu if the menu icon is clicked.
      */
-    menuIcon.click(function() {
+    menuIcon.click(function () {
       if (menu.css("visibility") === "hidden") {
         menu.css("visibility", "visible");
         menuIcon.addClass("active");
@@ -55,7 +59,7 @@ $(document).ready(function() {
      * Add a scroll listener to the menu to hide/show the navigation links.
      */
     if (menu.length) {
-      $(window).on("scroll", function() {
+      $(window).on("scroll", function () {
         var topDistance = menu.offset().top;
 
         // hide only the navigation links on desktop
@@ -67,10 +71,10 @@ $(document).ready(function() {
 
         // on tablet, hide the navigation icon as well and show a "scroll to top
         // icon" instead
-        if ( ! $( "#menu-icon" ).is(":visible") && topDistance < 50 ) {
+        if (!$("#menu-icon").is(":visible") && topDistance < 50) {
           $("#menu-icon-tablet").show();
           $("#top-icon-tablet").hide();
-        } else if (! $( "#menu-icon" ).is(":visible") && topDistance > 100) {
+        } else if (!$("#menu-icon").is(":visible") && topDistance > 100) {
           $("#menu-icon-tablet").hide();
           $("#top-icon-tablet").show();
         }
@@ -81,12 +85,12 @@ $(document).ready(function() {
      * Show mobile navigation menu after scrolling upwards,
      * hide it again after scrolling downwards.
      */
-    if ($( "#footer-post").length) {
+    if ($("#footer-post").length) {
       var lastScrollTop = 0;
-      $(window).on("scroll", function() {
+      $(window).on("scroll", function () {
         var topDistance = $(window).scrollTop();
 
-        if (topDistance > lastScrollTop){
+        if (topDistance > lastScrollTop) {
           // downscroll -> show menu
           $("#footer-post").hide();
         } else {
@@ -100,7 +104,7 @@ $(document).ready(function() {
         $("#toc-footer").hide();
         $("#share-footer").hide();
 
-        // show a "navigation" icon when close to the top of the page, 
+        // show a "navigation" icon when close to the top of the page,
         // otherwise show a "scroll to the top" icon
         if (topDistance < 50) {
           $("#actions-footer > #top").hide();
@@ -110,4 +114,138 @@ $(document).ready(function() {
       });
     }
   }
+
+
+
+  // firstImpression libray - move this to a separate file
+  /*
+   * firstImpression.js
+   * Copyright (c) 2012 Rob Flaherty (@robflaherty)
+   * Licensed under the MIT and GPL licenses.
+   */
+
+  window.firstImpression = function (cookie, days) {
+
+    var cookieMachine, getCookie, setCookie, checkUser;
+
+    /* Plain JS port of jquery.cookie plugin
+     * Copyright (c) 2010 Klaus Hartl (stilbuero.de)
+     * Dual licensed under the MIT and GPL licenses.
+     */
+
+    cookieMachine = function (key, value, options) {
+      var expiration, result, time;
+
+      if (arguments.length > 1 && String(value) !== "[object Object]") {
+        options = options || {};
+
+        if (value === null || value === undefined) {
+          options.expires = -1;
+        }
+
+        if (typeof options.expires === 'number') {
+          expiration = options.expires;
+          time = options.expires = new Date();
+          time.setTime(time.getTime() + expiration * 24 * 60 * 60 * 1000);
+        }
+
+        // Temporary fix for path problem
+        options.path = '/';
+
+        return (document.cookie = [
+          encodeURIComponent(key),
+          '=',
+          encodeURIComponent(value),
+          options.expires ? '; expires=' + options.expires.toUTCString() : '',
+          options.path ? '; path=' + options.path : '',
+          options.domain ? '; domain=' + options.domain : '',
+          options.secure ? '; secure' : ''
+        ].join(''));
+      }
+
+      result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie);
+      return result ? decodeURIComponent(result[1]) : null;
+    };
+
+    /*
+     * Option defaults
+     */
+
+    if (cookie === undefined) {
+      cookie = "_firstImpression";
+    }
+
+    if (days === undefined) {
+      days = 730;
+    }
+
+    /*
+     * Delete cookie if either option is null
+     */
+
+    if (cookie === null) {
+      cookieMachine("_firstImpression", null);
+      return;
+    }
+
+    if (days === null) {
+      cookieMachine(cookie, null);
+      return;
+    }
+
+    /*
+     * Functions
+     */
+
+    getCookie = function () {
+      return cookieMachine(cookie);
+    };
+
+    setCookie = function () {
+      cookieMachine(cookie, true, { expires: days });
+    };
+
+    checkUser = function () {
+      var status = getCookie();
+
+      // Set cookie if new user
+      if (!status) {
+        setCookie();
+      }
+
+      return !status;
+    };
+
+    /*
+     * Return boolean
+     */
+
+    return checkUser();
+
+  };
+
+
+
+
+
+
+  // show/hide first_time_here_giff logic
+  var first_time_here_gif = $("#first_time_here_gif");
+
+
+  // Basic usage
+  if (firstImpression()) {
+    console.log('New user');
+    first_time_here_gif.show();
+    setTimeout(function() {
+      first_time_here_gif.fadeOut('fast');
+  }, 8000);
+
+  }
+  else { console.log('This user has been here before');
+  first_time_here_gif.hide();
+}
+
+
+
 });
